210cfb19ec30a0ca39563abb47c78a20c420b84d
hsynarsln <90923579+hsynarsln@users.noreply.github.com>
2021-12-12 23:15:16 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,20 +16,16 @@ const Header = ({ title = 'Task Tracker', toggleShow, showAddTask }) => {
       <h1>{title}</h1>
       {/* <h1>{title}</h1> */}
 
-      {/* //? handleClick eventini gönderiyoruz. BestPractice olarak Button.js'e aynı adlı gönderip aynı adla almak gerekir. */}
       <Button color={showAddTask ? 'red' : 'purple'} text={toggleShow ? 'Close Add Task Bar' : 'Show Add Task Bar'} toggleShow={toggleShow} />
     </header>
   );
 };
 
-//? Parent componentten herhangi bir props gelmiyor ise aşağıdaki title alır
+
 Header.defaultProps = {
   title: 'Task Tracker'
 };
 
-//? parant componentten gelecek olan prop için string olması gerekli gibi
-// Header.propTypes = {
-//   title: PropTypes.string.isRequired,
-// };
+
 
 export default Header;
